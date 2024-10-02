029230c4c01350b5718734cbca8826de58cf24c6
vampirnp <vampir1122332@bk.ru>
2024-10-02 11:29:24 +0000

    добавлен recert_string.c, swap.c, history_lab2.txt.

--- lab2/src/revert_string/revert_string.c
+++ lab2/src/revert_string/revert_string.c
@@ -1,7 +1,12 @@
 #include "revert_string.h"
+#include <string.h>
 
 void RevertString(char *str)
 {
-	// your code here
+    int len = strlen(str);
+    for (int i = 0; i < len / 2; i++) {
+        char temp = str[i];
+        str[i] = str[len - i - 1];
+        str[len - i - 1] = temp;
+    }
 }
-
