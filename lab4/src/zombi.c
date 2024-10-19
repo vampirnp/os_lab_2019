0de3424369d6b9694735ecc01c25ff5c85939191
vampirnp <vampir1122332@bk.ru>
2024-10-19 13:31:38 +0000

    modified:   lab4/src/zombi.c

--- lab4/src/zombi.c
+++ lab4/src/zombi.c
@@ -1,18 +1,34 @@
+#include <stdio.h>
 #include <stdlib.h>
-#include <sys/types.h>
 #include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
 
 int main() {
-    // Создание дочернего процесса
-    
-    pid_t childPid = fork();
-    if (childPid > 0) {
-        // Ставим родительский процесс на минутную паузу
-        sleep(10);
-    }
-    else {
-        //Завершаем работу дочернего процесса
+    pid_t pid;
+
+    // Создаем дочерний процесс
+    pid = fork();
+
+    if (pid < 0) {
+        // Ошибка при создании процесса
+        perror("fork failed");
+        exit(1);
+    } else if (pid == 0) {
+        // Дочерний процесс
+        printf("Child process (PID: %d) is running...\n", getpid());
+        sleep(5);
+        printf("Child process (PID: %d) is exiting...\n", getpid());
         exit(0);
+    } else {
+        // Родительский процесс
+        printf("Parent process (PID: %d) is running...\n", getpid());
+        sleep(10);
+        printf("Parent process (PID: %d) is exiting...\n", getpid());
+
+        // Родительский процесс вызывает wait для получения статуса завершения дочернего процесса
+       // wait(NULL);
     }
+
     return 0;
 }
